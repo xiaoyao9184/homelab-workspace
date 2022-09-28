224c5b193d3b0a9bd61d4478ebc50e0e0d2ffb16
xiaoyao9184 <xiaoyao9184@gmail.com>
2022-09-28 17:32:45 +0800

    Add rclone for remote dsm

---
+++ dsm/rclone/bash.deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+curl https://rclone.org/install.sh | sudo bash
+
+rclone config
+
+
+config_path=$(rclone config file | grep /)
+config_dir="$( cd "$( dirname "${config_path}" )" >/dev/null 2>&1 && pwd )"
+config_list=$(rclone config show | grep '\[' | sed -E 's/\[(.*)\]/\1/')
+config_count=$(echo "$config_list" | wc -l)
+
+if (( $config_count > 1 )); then
+  array=( $(config_list) )
+  select config_name in array
+  do
+    read -p "Input mount target path of $config_name: " config_mount
+  done
+else
+  config_name="$config_list"
+fi
+
+[[ -z "$config_mount" ]] && config_mount=$HOME/$config_name
+mkdir -p ${config_mount}
+
+
+script=$(cat << EOF
+rclone \
+ mount ${config_name}: /var/services/homes/xy/GDrive \
+ --config=$config_path \
+ --allow-other \
+ --allow-non-empty \
+ --buffer-size 32M \
+ --vfs-read-chunk-size=32M \
+ --vfs-read-chunk-size-limit 2048M \
+ --vfs-cache-mode writes \
+ --dir-cache-time 96h \
+ --daemon
+EOF
+)
+echo "$script" >> ${config_dir}/${config_name}.sh
+bash ${config_dir}/${config_name}.sh
+
+echo "You have script ${config_dir}/${config_name}.sh"
