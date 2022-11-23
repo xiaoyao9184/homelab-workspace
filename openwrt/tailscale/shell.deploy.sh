34cad61addb2d54c2dc122478fab44a82a35a216
xiaoyao9184 <xiaoyao9184@gmail.com>
2022-11-23 18:17:40 +0800

    Support tailscale from distfeeds

--- openwrt/tailscale/shell.deploy.sh
+++ openwrt/tailscale/shell.deploy.sh
@@ -1,21 +1,36 @@
 
 opkg update
-opkg install jq libustream-openssl ca-bundle kmod-tun
+#fw4
+if [[ -n "$(command -v fw4)" ]
+then
+    #nftables
+    opkg install iptables-nft
+fi
 
-# https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
-get_latest_release() {
-  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
-    grep '"tag_name":' |                                            # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
-}
-version=$(get_latest_release "adyanth/openwrt-tailscale-enabler")
+from_distfeeds=$(opkg list | grep tailscale)
 
-curl -L https://github.com/adyanth/openwrt-tailscale-enabler/releases/download/$version/openwrt-tailscale-enabler-$version.tgz > openwrt-tailscale-enabler-$version.tgz
-tar x -zvC / -f openwrt-tailscale-enabler-$version.tgz
+if [ -z "$from_distfeeds" ]
+then
+    opkg update
+    opkg install jq libustream-openssl ca-bundle kmod-tun
 
-/etc/init.d/tailscale start
-/etc/init.d/tailscale enable
-ls /etc/rc.d/S*tailscale*
+    # https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
+    get_latest_release() {
+        curl --silent "https://api.github.com/repos/$1/releases/latest" |   # Get latest release from GitHub api
+            grep '"tag_name":' |                                            # Get tag line
+            sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+    }
+    version=$(get_latest_release "adyanth/openwrt-tailscale-enabler")
+
+    curl -L https://github.com/adyanth/openwrt-tailscale-enabler/releases/download/$version/openwrt-tailscale-enabler-$version.tgz > openwrt-tailscale-enabler-$version.tgz
+    tar x -zvC / -f openwrt-tailscale-enabler-$version.tgz
+
+    /etc/init.d/tailscale start
+    /etc/init.d/tailscale enable
+    ls /etc/rc.d/S*tailscale*
+else
+    opkg install tailscale
+fi
 
 net_lan=$(ip route | grep src | grep br-lan | awk '{print $1}')
 tailscale up --advertise-routes=$net_lan
--- openwrt/tailscale/shell.dispose.sh
+++ openwrt/tailscale/shell.dispose.sh
@@ -12,10 +12,17 @@ tailscale down
 /etc/init.d/tailscale disable
 ls /etc/rc.d/S*tailscale*
 
-kill $(ps | grep '[p]/etc/config/tailscaled.state' | awk '{print $1}')
+from_distfeeds=$(opkg list-installed | grep tailscale)
 
-rm /etc/config/tailscaled.state
-rm /etc/init.d/tailscale
-rm /usr/bin/tailscale
-rm /usr/bin/tailscaled
-rm /tmp/tailscale.up.log
+if [ -z "$from_distfeeds" ]
+then
+    kill $(ps | grep '[p]/etc/config/tailscaled.state' | awk '{print $1}')
+
+    rm /etc/config/tailscaled.state
+    rm /etc/init.d/tailscale
+    rm /usr/bin/tailscale
+    rm /usr/bin/tailscaled
+    rm /tmp/tailscale.up.log
+else
+    opkg uninstall tailscale
+fi
