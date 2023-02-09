6a4ca8e1c198b5ffb31087842471d1d0a24af41b
xiaoyao9184 <xiaoyao9184@gmail.com>
2023-02-09 11:33:30 +0800

    Update partition and rootfs for openwrt

--- openwrt/partition/shell.resize.sh
+++ openwrt/partition/shell.resize.sh
@@ -1,26 +1,31 @@
-# https://openwrt.org/docs/guide-user/installation/openwrt_x86#resizing_ext4_rootfs
-# https://oriolrius.cat/2022/08/04/resize-squasfs-ext4-partition-of-openwrt-in-a-raspberry-pi/
+# https://openwrt.org/docs/guide-user/installation/openwrt_x86#resizing_partitions
 
 opkg update
-opkg install lsblk losetup resize2fs
+opkg install parted lsblk
 
 BOOT="$(sed -n -e "\|\s/boot\s.*$|{s///p;q}" /etc/mtab)"
-DISK="${BOOT%%[0-9]*}"
-PART="$((${BOOT##*[^0-9]}+1))"
-ROOT="${DISK}${PART}"
-LOOP="$(losetup -f)"
 
-SQUASHFS_ROM="$(lsblk -n -i -r -o NAME,MOUNTPOINT,FSTYPE | grep 'squashfs' | grep '/rom' | awk '{print $1}')"
-
-if [ -z "$SQUASHFS_ROM" ]
+if [ -z "$BOOT" ]
 then
-    losetup ${LOOP} ${ROOT}
+    ROOT_NAME="$(lsblk -n -i -r -o NAME,MOUNTPOINT,FSTYPE | grep 'ext4' | grep '/' | awk '{print $1}')"
+    DISK="/dev/$(lsblk -no pkname /dev/${ROOT_NAME})"
+    PART="$(cat /proc/partitions | grep ${ROOT_NAME} | tail -1 | awk '{print $2}')"
 else
-    # loop_off="$(losetup | grep $SQUASHFS_ROM | awk '{print $3}')"
-    # losetup -o ${loop_off} ${LOOP} ${ROOT}
-    LOOP="$(losetup -n -l | sed -n -e "\|\s.*\s${ROOT#/dev}\s.*$|{s///p;q}")"
+    DISK="${BOOT%%[0-9]*}"
+    PART="$((${BOOT##*[^0-9]}+1))"
 fi
 
-# fsck.ext4 -y ${LOOP}
-resize2fs ${LOOP}
+echo Fix | parted -l
+parted ${DISK} resizepart ${PART} 100%
+
+TABLE="$(parted -l | grep '^Partition Table' | awk '{split($0,t,": ");print t[2]}')"
+if [ "$TABLE"=="gpt" ]
+then
+    lsblk -n -o NAME,MOUNTPOINT,PARTUUID
+
+    ROOT="${DISK}${PART}"
+    UUID="$(lsblk -n -o PARTUUID ${ROOT})"
+    sed -i -r -e "s|(PARTUUID=)\S+|\1${UUID}|g" /boot/grub/grub.cfg
+fi
+
 reboot
---
+++ openwrt/rootfs/shell.resize.sh
@@ -0,0 +1,48 @@
+# https://openwrt.org/docs/guide-user/installation/openwrt_x86#resizing_ext4_rootfs
+# https://oriolrius.cat/2022/08/04/resize-squasfs-ext4-partition-of-openwrt-in-a-raspberry-pi/
+
+opkg update
+opkg install lsblk losetup resize2fs
+
+LOOP="$(losetup -f)"
+BOOT="$(sed -n -e "\|\s/boot\s.*$|{s///p;q}" /etc/mtab)"
+if [ -z "$BOOT" ]
+then
+    ROOT="/dev/$(lsblk -n -i -r -o NAME,MOUNTPOINT,FSTYPE | grep 'ext4' | grep '/' | awk '{print $1}')"
+
+    echo "no boot partition in /etc/mtab!!! but root partition maybe: $ROOT"
+    read -p "do you want to proceed? (yes/no) " yn
+    case $yn in 
+        yes | y )
+            ;;
+        no | n ) 
+            echo "exiting..."
+            exit
+            ;;
+        * ) 
+            echo "invalid response"
+            exit 1
+            ;;
+    esac
+    
+    losetup ${LOOP} ${ROOT}
+    fsck.ext4 -y ${LOOP}
+else
+    DISK="${BOOT%%[0-9]*}"
+    PART="$((${BOOT##*[^0-9]}+1))"
+    ROOT="${DISK}${PART}"
+
+    SQUASHFS_ROM="$(lsblk -n -i -r -o NAME,MOUNTPOINT,FSTYPE | grep 'squashfs' | grep '/rom' | awk '{print $1}')"
+
+    if [ -z "$SQUASHFS_ROM" ]
+    then
+        losetup ${LOOP} ${ROOT}
+    else
+        # loop_off="$(losetup | grep $SQUASHFS_ROM | awk '{print $3}')"
+        # losetup -o ${loop_off} ${LOOP} ${ROOT}
+        LOOP="$(losetup -n -l | sed -n -e "\|\s.*\s${ROOT#/dev}\s.*$|{s///p;q}")"
+    fi
+fi
+
+resize2fs ${LOOP}
+reboot
