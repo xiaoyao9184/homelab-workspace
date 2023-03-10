399c27b91d5e7aff6df8756d4a4ec28e0e1eb254
xiaoyao9184 <xiaoyao9184@gmail.com>
2023-03-10 09:12:45 +0800

    Add hassio for linux

---
+++ linux/hassio/install.sh
@@ -0,0 +1,7 @@
+
+# This is the list of packages you need to have available on your system that will run Hass.io if you are using Debian/Ubuntu:
+sudo apt-get install apparmor-utils apt-transport-https avahi-daemon ca-certificates curl 
+dbus jq network-manager socat software-properties-common
+
+#To perform the Hass.io installation, run the following command as root:
+$ curl -sL https://raw.githubusercontent.com/home-assistant/hassio-build/master/install/hassio_install | bash -s
