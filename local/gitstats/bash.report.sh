fb8ceae84c0a0ba570c865dd4243bee0a341eee7
xiaoyao9184 <xiaoyao9184@gmail.com>
2023-06-07 15:55:15 +0800

    Add gitstats report

---
+++ local/gitstats/bash.report.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+current_path=$(pwd)
+[[ -z "$mapping_stats" ]] && mapping_stats=${current_path}/../../git_stats.csv
+
+while IFS="," read -r report prefix_date prefix_delimiter suffix_date suffix_delimiter repo name
+do
+    report=$(echo "$report" | tr -d '"' | tr -d '\r' )
+    repo=$(echo "$repo" | tr -d '"' | tr -d '\r' )
+    name=$(echo "$name" | tr -d '"' | tr -d '\r' )
+    prefix_date=$(echo "$prefix_date" | tr -d '"' | tr -d '\r' )
+    prefix_delimiter=$(echo "$prefix_delimiter" | tr -d '"' | tr -d '\r' )
+    suffix_date=$(echo "$suffix_date" | tr -d '"' | tr -d '\r' )
+    suffix_delimiter=$(echo "$suffix_delimiter" | tr -d '"' | tr -d '\r' )
+
+    if [[ -z "$report" ]] || [[ -z "$repo" ]]
+    then
+        continue
+    fi
+
+    if [[ -z "$name" ]]
+    then
+        name=$(basename $repo)
+    fi
+    
+    if [[ "$prefix_date" == "yes" ]]
+    then
+        date=$(date '+%Y-%m-%d')
+        groupname="${date}${prefix_delimiter}${name}"
+    fi
+
+    if [[ "$suffix_date" == "yes" ]]
+    then
+        date=$(date '+%Y-%m-%d')
+        groupname="${name}${suffix_delimiter}${date}"
+    fi
+
+    output=${report}/${groupname}
+    mkdir -p ${output}
+
+    echo "$repo -> $output"
+    echo ""
+    docker run \
+        --rm \
+        -v ${repo}:/git-projects/default \
+        -v ${output}:/output-reports/default \
+        -e GITSTATS_CONFIG_PROJECT_NAME=${name} \
+        xiaoyao9184/docker-gitstats:latest
+
+done < <(tail -n +2 ${mapping_stats})
+
