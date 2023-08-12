8a5149efce9b830951a4c414449caabcd1a0cb6a
xiaoyao9184 <xiaoyao9184@gmail.com>
2023-08-12 19:26:56 +0800

    Update configure-host for edgeos

--- edgeos/configure-host/bash.build.sh
+++ edgeos/configure-host/bash.build.sh
@@ -2,11 +2,20 @@
 current_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 [[ -z "$build_path" ]] && build_path=${current_path}/build/
 [[ -z "$build_name" ]] && build_name=edgeos_configure_static_host_mapping.sh
-[[ -z "$mapping_dhcp" ]] && mapping_dhcp=${current_path}/dhcp_host.csv
-[[ -z "$mapping_domain" ]] && mapping_domain=${current_path}/domain_ip.csv
+[[ -z "$mapping_csv" ]] && mapping_csv=${current_path}/host.csv
+[[ -z "$mapping_column" ]] && mapping_column="location name ip"
 
 build_extension="${build_name##*.}"
 build_filename="${build_name%.*}"
+
+echo "The following is the index of field in the column:"
+read -a columns <<< "$mapping_column"
+for index in "${!columns[@]}";
+do
+    [[ "${columns[$index]}" == "location" ]] && column_location=$index && echo "  ${columns[$index]} -> $index"
+    [[ "${columns[$index]}" == "ip" ]] && column_ip=$index && echo "  ${columns[$index]} -> $index"
+    [[ "${columns[$index]}" == "name" ]] && column_name=$index && echo "  ${columns[$index]} -> $index"
+done
 
 rm -rdf ${build_path}
 mkdir -p ${build_path}
@@ -20,15 +29,15 @@ commit
 save
 EOT
 
+echo "The following location script will be created:"
 # read all location
 declare -a locations
-while IFS="," read -r location tag leasetime ip_addr mac cl_name comment
+while IFS="," read -ra rows
 do
-    location=$(echo "$location" | tr -d '"' | tr -d '\r' )
+    location=$(echo "${rows[$column_location]}" | tr -d '"' | tr -d '\r' )
     locations+=("$location")
-done < <(tail -n +2 ${mapping_dhcp})
+done < <(tail -n +2 ${mapping_csv})
 sorted_unique_locations=($(echo "${locations[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-echo "The following location script will be created: ${sorted_unique_locations[@]}"
 
 # output by location
 for location in "${sorted_unique_locations[@]}"
@@ -36,91 +45,34 @@ do
     build_file=${build_path}/${build_filename}@${location}.${build_extension}
     echo "configure" >> ${build_file}
     echo "" >> ${build_file}
+    echo "  $location -> $build_file"
 done
 
-while IFS="," read -r location tag leasetime ip_addr mac cl_name comment
+echo "The following items will be out to script file:"
+while IFS="," read -ra rows
 do
-    location=$(echo "$location" | tr -d '"' | tr -d '\r' )
-    name=$(echo "$cl_name" | tr -d '"' | tr -d '\r' )
-    ip=$(echo "$ip_addr" | tr -d '"' | tr -d '\r' )
-    echo "$ip - $name"
+    location=$(echo "${rows[$column_location]}" | tr -d '"' | tr -d '\r' )
+    name=$(echo "${rows[$column_name]}" | tr -d '"' | tr -d '\r' )
+    ip=$(echo "${rows[$column_ip]}" | tr -d '"' | tr -d '\r' )
 
-    template=$(cat << EOF
-delete system static-host-mapping host-name ${name}.lan
+    if [[ "$location" && "$name" && "$ip" ]]
+    then
+        echo "  $ip - $name/$name.lan"
+
+        template=$(cat << EOF
+set system static-host-mapping host-name ${name} inet ${ip}
 set system static-host-mapping host-name ${name}.lan inet ${ip}
 commit
 EOF
 )
-    if [[ "$name" ]]
-    then
         build_file=${build_path}/${build_filename}@${location}.${build_extension}
         echo "$template" >> ${build_file}
     fi
-done < <(tail -n +2 ${mapping_dhcp})
+done < <(tail -n +2 ${mapping_csv})
 
 for location in "${sorted_unique_locations[@]}"
 do
     build_file=${build_path}/${build_filename}@${location}.${build_extension}
-    echo "" >> ${build_file}
-    echo "save" >> ${build_file}
-done
-
-
-# read all where
-declare -a wheres
-while IFS="," read -r where ip_addr cl_name comment edgeos_domain
-do
-    where=$(echo "$where" | tr -d '"' | tr -d '\r' )
-    wheres+=("$where")
-done < <(tail -n +2 ${mapping_domain})
-sorted_unique_wheres=($(echo "${wheres[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-echo "The following location script will be created: ${sorted_unique_wheres[@]}"
-
-# output by where
-for where in "${sorted_unique_wheres[@]}"
-do
-    build_file=${build_path}/${build_filename}@${where}.${build_extension}
-    rm -f ${build_file}
-    echo "configure" >> ${build_file}
-    echo "" >> ${build_file}
-done
-
-while IFS="," read -r where ip_addr cl_name comment edgeos_domain
-do
-    where=$(echo "$where" | tr -d '"' | tr -d '\r' )
-    name=$(echo "$cl_name" | tr -d '"' | tr -d '\r' )
-    ip=$(echo "$ip_addr" | tr -d '"' | tr -d '\r' )
-    edgeos_domain=$(echo "$edgeos_domain" | tr -d '"' | tr -d '\r')
-    echo "$ip - $name - @$where"
-
-    template=$(cat << EOF
-delete system static-host-mapping host-name ${name}
-set system static-host-mapping host-name ${name} inet ${ip}
-commit
-EOF
-)
-    if [[ "$name" ]]
-    then
-        build_file=${build_path}/${build_filename}@${where}.${build_extension}
-        echo "$template" >> ${build_file}
-    fi
-
-    template=$(cat << EOF
-delete system static-host-mapping host-name ${name}${edgeos_domain}
-set system static-host-mapping host-name ${name}${edgeos_domain} inet ${ip}
-commit
-EOF
-)
-    if [[ "$edgeos_domain" ]]
-    then
-        build_file=${build_path}/${build_filename}@${where}.${build_extension}
-        echo "$template" >> ${build_file}
-    fi
-done < <(tail -n +2 ${mapping_domain})
-
-for where in "${sorted_unique_wheres[@]}"
-do
-    build_file=${build_path}/${build_filename}@${where}.${build_extension}
     echo "" >> ${build_file}
     echo "save" >> ${build_file}
 done
