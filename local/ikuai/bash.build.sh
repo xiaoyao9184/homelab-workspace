2aabb2ef6d7b23ac25ee89158c17a45fa6a9279f
xiaoyao9184 <xiaoyao9184@gmail.com>
2023-08-13 15:36:29 +0800

    Update ikuai for local

--- local/ikuai/bash.build.sh
+++ local/ikuai/bash.build.sh
@@ -1,51 +1,62 @@
 #!/bin/bash
 current_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-[[ -z "$build_path" ]] && build_path=${current_path}/hosts/
+[[ -z "$build_path" ]] && build_path=${current_path}/build/
 [[ -z "$build_name" ]] && build_name=ikuai_dhcp_static.csv
-[[ -z "$mapping_dhcp" ]] && mapping_dhcp=${current_path}/dhcp_host.csv
+[[ -z "$mapping_csv" ]] && mapping_csv=${current_path}/dhcp.csv
+[[ -z "$mapping_column" ]] && mapping_column="location ip mac name"
 
 build_extension="${build_name##*.}"
 build_filename="${build_name%.*}"
 
+echo "The following is the index of field in the column:"
+read -a columns <<< "$mapping_column"
+for index in "${!columns[@]}";
+do
+    [[ "${columns[$index]}" == "location" ]] && column_location=$index && echo "  ${columns[$index]} -> $index"
+    [[ "${columns[$index]}" == "ip" ]] && column_ip=$index && echo "  ${columns[$index]} -> $index"
+    [[ "${columns[$index]}" == "mac" ]] && column_mac=$index && echo "  ${columns[$index]} -> $index"
+    [[ "${columns[$index]}" == "name" ]] && column_name=$index && echo "  ${columns[$index]} -> $index"
+    [[ "${columns[$index]}" == "comment" ]] && column_comment=$index && echo "  ${columns[$index]} -> $index"
+done
+
 rm -rdf ${build_path}
 mkdir -p ${build_path}
 
+echo "The following location script will be created:"
 # read all location
 declare -a locations
-while IFS="," read -r location tag leasetime ip_addr mac cl_name comment
+while IFS="," read -ra rows
 do
-    location=$(echo "$location" | tr -d '"' | tr -d '\r' )
+    location=$(echo "${rows[$column_location]}" | tr -d '"' | tr -d '\r' )
     locations+=("$location")
-done < <(tail -n +2 ${mapping_dhcp})
+done < <(tail -n +2 ${mapping_csv})
 sorted_unique_locations=($(echo "${locations[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-echo "The following location script will be created: ${sorted_unique_locations[@]}"
 
 # output by location
 for location in "${sorted_unique_locations[@]}"
 do
     build_file=${build_path}/${build_filename}@${location}.${build_extension}
     echo "enabled,interface,ip_addr,mac,cl_name,comment" >> ${build_file}
+    echo "  $location -> $build_file"
 done
 
-while IFS="," read -r location tag leasetime ip_addr mac cl_name comment
+echo "The following items will be out to script file:"
+while IFS="," read -ra rows
 do
-    location=$(echo "$location" | tr -d '"' | tr -d '\r' )
-    comment=$(echo "$comment" | tr -d '"' | tr -d '\r' )
-    name=$(echo "$cl_name" | tr -d '"' | tr -d '\r' )
-    mac=$(echo "$mac" | tr -d '"' | tr -d '\r' )
-    ip=$(echo "$ip_addr" | tr -d '"' | tr -d '\r' )
-    echo "$ip - $name - $mac - $comment"
+    location=$(echo "${rows[$column_location]}" | tr -d '"' | tr -d '\r' )
+    ip=$(echo "${rows[$column_ip]}" | tr -d '"' | tr -d '\r' )
+    mac=$(echo "${rows[$column_mac]}" | tr -d '"' | tr -d '\r' )
+    name=$(echo "${rows[$column_name]}" | tr -d '"' | tr -d '\r' )
+    [[ "$column_comment" ]] && comment=$(echo "${rows[$column_comment]}" | tr -d '"' | tr -d '\r' )
 
-    template=$(cat << EOF
-"yes","auto","${ip}","${mac}","${name}","${comment}"
-EOF
-)
-    if [[ "$name" ]]
+    if [[ "$location" && "$ip" && "$mac" && "$name" ]]
     then
         build_file=${build_path}/${build_filename}@${location}.${build_extension}
-        echo "$template" >> ${build_file}
+        echo "  $ip - $mac - $name"
+
+        echo ""yes","auto","${ip}","${mac}","${name}","${comment}"" >> ${build_file}
     fi
-done < <(tail -n +2 ${mapping_dhcp})
+done < <(tail -n +2 ${mapping_csv})
 
 for location in "${sorted_unique_locations[@]}"
 do
