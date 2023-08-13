dadb36b05cd76058d39c66cba1f12ac0bd6ae2f9
xiaoyao9184 <xiaoyao9184@gmail.com>
2023-08-13 13:40:54 +0800

    Update uci-dhcp.domain

--- openwrt/uci-dhcp/domain/bash.build.sh
+++ openwrt/uci-dhcp/domain/bash.build.sh
@@ -2,11 +2,20 @@
 current_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 [[ -z "$build_path" ]] && build_path=${current_path}/build/
 [[ -z "$build_name" ]] && build_name=wrt_uci_dhcp_domain.sh
-[[ -z "$mapping_dhcp" ]] && mapping_dhcp=${current_path}/dhcp_host.csv
-[[ -z "$mapping_domain" ]] && mapping_domain=${current_path}/domain_ip.csv
+[[ -z "$mapping_csv" ]] && mapping_csv=${current_path}/dhcp_domain.csv
+[[ -z "$mapping_column" ]] && mapping_column="location ip domain"
 
 build_extension="${build_name##*.}"
 build_filename="${build_name%.*}"
+
+echo "The following is the index of field in the column:"
+read -a columns <<< "$mapping_column"
+for index in "${!columns[@]}";
+do
+    [[ "${columns[$index]}" == "location" ]] && column_location=$index && echo "  ${columns[$index]} -> $index"
+    [[ "${columns[$index]}" == "domain" ]] && column_domain=$index && echo "  ${columns[$index]} -> $index"
+    [[ "${columns[$index]}" == "ip" ]] && column_ip=$index && echo "  ${columns[$index]} -> $index"
+done
 
 rm -rdf ${build_path}
 mkdir -p ${build_path}
@@ -16,43 +25,46 @@ build_file=${build_path}/${build_filename}.${build_extension}
 echo "while uci -q delete dhcp.@domain[0]; do :; done" >> ${build_file}
 echo "" >> ${build_file}
 
+echo "The following location script will be created:"
 # read all location
 declare -a locations
-while IFS="," read -r location tag leasetime ip_addr mac cl_name comment
+while IFS="," read -ra rows
 do
-    location=$(echo "$location" | tr -d '"' | tr -d '\r' )
+    location=$(echo "${rows[$column_location]}" | tr -d '"' | tr -d '\r' )
     locations+=("$location")
-done < <(tail -n +2 ${mapping_dhcp})
+done < <(tail -n +2 ${mapping_csv})
 sorted_unique_locations=($(echo "${locations[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-echo "The following location script will be created: ${sorted_unique_locations[@]}"
 
 # output by location
 for location in "${sorted_unique_locations[@]}"
 do
     build_file=${build_path}/${build_filename}@${location}.${build_extension}
     echo "" >> ${build_file}
+    echo "  $location -> $build_file"
 done
 
-while IFS="," read -r location tag leasetime ip_addr mac cl_name comment
+echo "The following items will be out to script file:"
+while IFS="," read -ra rows
 do
-    location=$(echo "$location" | tr -d '"' | tr -d '\r' )
-    name=$(echo "$cl_name" | tr -d '"' | tr -d '\r' )
-    mac=$(echo "$mac" | tr -d '"' | tr -d '\r' )
-    ip=$(echo "$ip_addr" | tr -d '"' | tr -d '\r' )
+    location=$(echo "${rows[$column_location]}" | tr -d '"' | tr -d '\r' )
+    domain=$(echo "${rows[$column_domain]}" | tr -d '"' | tr -d '\r' )
+    ip=$(echo "${rows[$column_ip]}" | tr -d '"' | tr -d '\r' )
 
-    template=$(cat << EOF
+    if [[ "$location" && "$domain" && "$ip" && "$ip" != "::" ]]
+    then
+        echo "  $ip - $domain"
+        
+        template=$(cat << EOF
 uci add dhcp domain
-uci set dhcp.@domain[-1].name='${name}'
+uci set dhcp.@domain[-1].name='${domain}'
 uci set dhcp.@domain[-1].ip='${ip}'
+uci reorder dhcp.@domain[-1]=0
 EOF
 )
-    if [[ "$name" ]]
-    then
-        echo "$ip - $name - @$location"
         build_file=${build_path}/${build_filename}@${location}.${build_extension}
         echo "$template" >> ${build_file}
     fi
-done < <(tail -n +2 ${mapping_dhcp})
+done < <(tail -n +2 ${mapping_csv})
 
 for location in "${sorted_unique_locations[@]}"
 do
@@ -60,45 +72,6 @@ do
     echo "" >> ${build_file}
     echo "uci commit" >> ${build_file}
 done
-
-
-# read all where
-declare -a wheres
-while IFS="," read -r where ip_addr cl_name comment edgeos_domain
-do
-    where=$(echo "$where" | tr -d '"' | tr -d '\r' )
-    wheres+=("$where")
-done < <(tail -n +2 ${mapping_domain})
-sorted_unique_wheres=($(echo "${wheres[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-echo "The following location script will be created: ${sorted_unique_wheres[@]}"
-
-# output by location
-for where in "${sorted_unique_wheres[@]}"
-do
-    build_file=${build_path}/${build_filename}@${where}.${build_extension}
-    echo "" >> ${build_file}
-done
-
-while IFS="," read -r where ip_addr cl_name comment edgeos_domain
-do
-    where=$(echo "$where" | tr -d '"' | tr -d '\r' )
-    name=$(echo "$cl_name" | tr -d '"' | tr -d '\r' )
-    ip=$(echo "$ip_addr" | tr -d '"' | tr -d '\r' )
-
-    template=$(cat << EOF
-uci add dhcp domain
-uci set dhcp.@domain[-1].name='${name}'
-uci set dhcp.@domain[-1].ip='${ip}'
-uci reorder dhcp.@domain[-1]=0
-EOF
-)
-    if [[ "$name" ]]
-    then
-        echo "$ip - $name - @$where"
-        build_file=${build_path}/${build_filename}@${where}.${build_extension}
-        echo "$template" >> ${build_file}
-    fi
-done < <(tail -n +2 ${mapping_domain})
 
 for where in "${sorted_unique_wheres[@]}"
 do
