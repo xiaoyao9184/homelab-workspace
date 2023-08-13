6d47606ff1f475bcc5561b696427959d0e1e8191
xiaoyao9184 <xiaoyao9184@gmail.com>
2023-08-13 14:39:02 +0800

    Update uci-dhcp.tag

--- openwrt/uci-dhcp/tag/bash.build.sh
+++ openwrt/uci-dhcp/tag/bash.build.sh
@@ -12,10 +12,20 @@
 current_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 [[ -z "$build_path" ]] && build_path=${current_path}/build/
 [[ -z "$build_name" ]] && build_name=dhcp_tag.sh
-[[ -z "$mapping_dhcp_tag" ]] && mapping_dhcp_tag=${current_path}/dhcp_tag.csv
+[[ -z "$mapping_csv" ]] && mapping_csv=${current_path}/dhcp_tag.csv
+[[ -z "$mapping_column" ]] && mapping_column="location tag ip"
 
 build_extension="${build_name##*.}"
 build_filename="${build_name%.*}"
+
+echo "The following is the index of field in the column:"
+read -a columns <<< "$mapping_column"
+for index in "${!columns[@]}";
+do
+    [[ "${columns[$index]}" == "location" ]] && column_location=$index && echo "  ${columns[$index]} -> $index"
+    [[ "${columns[$index]}" == "tag" ]] && column_tag=$index && echo "  ${columns[$index]} -> $index"
+    [[ "${columns[$index]}" == "ip" ]] && column_ip=$index && echo "  ${columns[$index]} -> $index"
+done
 
 rm -rdf ${build_path}
 mkdir -p ${build_path}
@@ -24,30 +34,36 @@ mkdir -p ${build_path}
 build_file=${build_path}/${build_filename}.${build_extension}
 echo "" >> ${build_file}
 
+echo "The following location script will be created:"
 # read all location
 declare -a locations
-while IFS="," read -r location tag ip_addr
+while IFS="," read -ra rows
 do
-    location=$(echo "$location" | tr -d '"' | tr -d '\r' )
+    location=$(echo "${rows[$column_location]}" | tr -d '"' | tr -d '\r' )
     locations+=("$location")
-done < <(tail -n +2 ${mapping_dhcp_tag})
+done < <(tail -n +2 ${mapping_csv})
 sorted_unique_locations=($(echo "${locations[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-echo "The following location script will be created: ${sorted_unique_locations[@]}"
 
 # output by location
 for location in "${sorted_unique_locations[@]}"
 do
     build_file=${build_path}/${build_filename}@${location}.${build_extension}
     echo "" >> ${build_file}
+    echo "  $location -> $build_file"
 done
 
-while IFS="," read -r location tag ip_addr
+echo "The following items will be out to script file:"
+while IFS="," read -ra rows
 do
-    location=$(echo "$location" | tr -d '"' | tr -d '\r' )
-    tag=$(echo "$tag" | tr -d '"' | tr -d '\r' )
-    ip=$(echo "$ip_addr" | tr -d '"' | tr -d '\r' )
+    location=$(echo "${rows[$column_location]}" | tr -d '"' | tr -d '\r' )
+    tag=$(echo "${rows[$column_tag]}" | tr -d '"' | tr -d '\r' )
+    ip=$(echo "${rows[$column_ip]}" | tr -d '"' | tr -d '\r' )
 
-    template=$(cat << EOF
+    if [[ "$location" && "$tag" && "$ip" ]]
+    then
+        echo "  $tag - $ip"
+
+        template=$(cat << EOF
 
 uci delete dhcp.${tag}
 uci set dhcp.${tag}="tag"
@@ -56,13 +72,10 @@ uci add_list dhcp.${tag}.dhcp_option="6,${ip}"
 uci set dhcp.${tag}.force='1'
 EOF
 )
-    if [[ "$tag" ]]
-    then
-        echo "$ip - $tag"
         build_file=${build_path}/${build_filename}@${location}.${build_extension}
         echo "$template" >> ${build_file}
     fi
-done < <(tail -n +2 ${mapping_dhcp_tag})
+done < <(tail -n +2 ${mapping_csv})
 
 for location in "${sorted_unique_locations[@]}"
 do
